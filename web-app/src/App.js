9d79bab7ff40359b4bc44134e9442dfdd1c3e6b7
Angelvb18 <angeljosevb12@gmail.com>
2022-11-18 11:26:11 -0600

    Plantilla ventanas en Appjs

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import './Hojas-de-estilo/bootstrap.min.css'
+import './Hojas-de-estilo/Navbar-With-Button-icons.css'
+import './Hojas-de-estilo/styles.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div class="estiloHeading">
+        <h3 class="display-5">Programa</h3>
     </div>
+    <nav class="navbar navbar-light navbar-expand-md py-3" id="BarraHerramientas">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a class="nav-link active" href="#">First Item</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Second Item</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Third Item</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="MenuVentanas">
+        <div id="MenuDiv">
+            <div id="MenuTitulo">
+                <p class="TxtMenuTitulo"><strong>MENU</strong></p>
+            </div>
+            <div id="MenuElementos" class="claseMenuElementos">
+                <div id="Elemento1">
+                    <p class="TxtElements"><strong>Elemento</strong></p>
+                </div>
+                <div id="Elemento3">
+                    <p class="TxtElements"><strong>Elemento</strong></p>
+                </div>
+                <div id="Elemento2">
+                    <p class="TxtElements"><strong>Elemento</strong></p>
+                </div>
+                <div id="Elemento4">
+                    <p class="TxtElements"><strong>Elemento</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="Ventanas">
+            <div id="TituloVentana1">
+                <p class="TxtMenuTitulo"><strong>Descripcion De Ventana</strong></p>
+            </div>
+            <div id="Ventana1">
+                 <div id="ventanaText"></div>
+            </div>
+            <div id="TituloVentana2">
+                <p class="TxtMenuTitulo"><strong>Descripcion De Ventana</strong></p>
+            </div>
+            <div id="Ventana2">
+                <div id="ventana2Text"></div>
+            </div>
+        </div>
+    </div>
+</body>
   );
 }
 
