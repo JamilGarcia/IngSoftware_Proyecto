8d6d3f498434562e3970909132023f6a18c657c1
Angelvb18 <angeljosevb12@gmail.com>
2022-11-25 23:04:25 -0600

    intento_heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-
+const {config} = require('dotenv');
+const PORT = process.env.PORT
 //Middleware
 app.use(express.json());
 app.use(cors());//Permite comunicar servidor front-end y servidor back-end
@@ -10,6 +11,6 @@ const routePrueba = require('./routes/Login');
 //initialize routes
 app.use("/",routePrueba);
 
-app.listen(5000, () => {
-    console.log("listening on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server empezado en el puerto ${PORT}`);
   }) 
