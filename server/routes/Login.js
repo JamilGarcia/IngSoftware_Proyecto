afb43ab55ef982eaa99f4c959e6c2309459fd5d0
miguel <miguel@Miguels-MacBook-Air.local>
2022-11-28 13:26:26 -0600

    Ajustes de archivos front-end:

--- server/routes/Login.js
+++
@@ -1,7 +0,0 @@
-const {  verifyUser } = require('../controladores/Login_Controller');
-const router = require('express').Router();
-//router.get('/getUsers',getUsers);
-
-router.post("/", verifyUser);
-
-module.exports = router;
