bcf356b32ae816e429fb0d5ce2a741bbefbe6277
miguel <miguel@Miguels-MacBook-Air.local>
2022-12-01 13:57:55 -0600

    Ajuste de bugs en login e inclusion de validacion de correo valido

--- web-app/src/Componentes/Pantallas/LoginScreen.js
+++ web-app/src/Componentes/Pantallas/LoginScreen.js
@@ -8,7 +8,10 @@ const LoginScreen = () => {
     //Destructuring de datos de entrada, correo y contraseña
     const [entradasLogin,setEntradasLogin] = useState({login_Email: '',login_Password: ''});
     const {login_Email, login_Password} = entradasLogin;
-    const [formErrors, setFormErrors] = useState({errorEmail: '',errorPassword: ''});
+    const initialStateErrors = {
+        errorEmail: "", errorPassword: ""
+    }   
+    const [formErrors, setFormErrors] = useState(initialStateErrors);
     const {loginUsuario} = useContext(AuthContext);
 
     const onChange = (e) => {/*Metodo para manejar cambios en inputs de texto */
@@ -18,35 +21,46 @@ const LoginScreen = () => {
     /*Metodo para manejar datos de ingreso de sesión*/ 
     const manejarIngreso = (e) => {
         e.preventDefault();//Prevenir que recargue la pagina
-        // const {login_Email, login_Password};
         try {
-            setFormErrors({...validarValor()});
-            //Verificacion que no existan errores (verificacion de campos por bugs con errores)
-            if((formErrors.errorEmail === '' && formErrors.errorPassword === '')  &&
-                (login_Email !== '' && login_Password !== '')){
-                loginUsuario(login_Email, login_Password);//Llamado a metodo que conecta con el back-end
-            }
+            let resultadoVal = validarValor();
+                //Verificacion que no existan errores (verificacion de campos por bugs con errores)
+                if(resultadoVal){
+                    setFormErrors({errorEmail: "", errorPassword: ""});
+                    loginUsuario(login_Email, login_Password);//Llamado a metodo que conecta con el back-end
+                } 
         } catch (err) {
             console.error(err.message);
         }
     };
-
+    
     const validarValor = () => {
         /*Metodo para validacion de entradas, validacion de campos vacios y correo por regex */
-        const lista_errores = {errorEmail: '', errorPassword: ''}
-        const regex_Correo = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const regex_Correo = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        let validacion = true;
+        const lista_errores = {errorEmail: "", errorPassword: ""};
         if(login_Email.length === 0){
+           
+            validacion = false;
             lista_errores.errorEmail = "¡Se requiere ingresar una correo!";
-        } else if(!regex_Correo.test(login_Email)){
-            lista_errores.errorEmail = "Este no es un correo valido.";
         } else {
-            lista_errores.errorEmail = '';
+
+            if(!regex_Correo.test(login_Email)){
+                validacion = false;
+                lista_errores.errorEmail = "¡El formato del correo ingresado no es valido!";
+            }
         }
-        if(login_Password.length === 0)
+        if(login_Password.length === 0){
             lista_errores.errorPassword = "¡Se requiere ingresar una contraseña!";
-        else 
-            lista_errores.errorPassword = '';
-        return lista_errores;
+            validacion = false;
+        }
+
+        if(validacion){
+            return true;
+        } else {
+            setFormErrors(lista_errores);
+            return false;
+        }
+        //setFormErrors({...lista_errores})
     };
 
     return (
@@ -58,9 +72,10 @@ const LoginScreen = () => {
                 <form className="contenedor_datos" onSubmit={manejarIngreso}>
                     <div className="form-group">
                         <AiOutlineMail size={35} color="gray"/>
-                        <input type = "email" placeholder ="Ingrese su correo" 
+                        <input type = "text" placeholder ="Ingrese su correo" 
                             id="email_login" name="login_Email" className="form-control" 
-                            value={login_Email} onChange={e => onChange(e)}/>
+                            value={login_Email} onChange={e => onChange(e)}   
+                        />
                     </div>
                     <p className="indicador_error">{formErrors.errorEmail}</p>
                     <div className ="form-group">
