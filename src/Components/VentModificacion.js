942f9076ee1bf7b36d5b30c00869022e927c1442
fernandoibarra015 <41806744+fernandoibarra015@users.noreply.github.com>
2022-12-05 21:20:28 -0600

    Nuevas ventanas de ver perfil y modificar

--- src/Components/VentModificacion.js
+++ src/Components/VentModificacion.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./VentModificacion.css";
 import avatar from "./avatar.png";
+import LogoComunicarte from "./LogoComunicarte.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -10,18 +11,16 @@ export const VentModificacion = () => {
   }
   const [Firstname, setFirstName] = useState("");
   const [Middlename, setMiddleName] = useState("");
-  const [FirstLastname, setFirstLastName] = useState("");
-  const [SecondLastname, setSecondLastName] = useState("");
+  const [Lastnames, setLastNames] = useState("");
   const [Correo, setCorreo] = useState("");
   const [FechaNacimiento, setFechaNacimiento] = useState("");
-  const [error, setError]=useState(false);
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (
       Firstname.length == 0 ||
-      FirstLastname.length == 0 ||
-      SecondLastname.length == 0 ||
+      Lastnames.length == 0 ||
       Correo.length == 0 ||
       FechaNacimiento.length == 0
     ) {
@@ -30,124 +29,183 @@ export const VentModificacion = () => {
   };
 
   return (
-    <div className="App">
-      {/*RECTANGULO SUPERIOR ROSA*/}
-      <header className="RectanguloSuperiorRosa"></header>
-
-      {/*CUADRO CENTRADO DONDE VA TODA LA DATA*/}
-      <div class="CuadroCentrado">
-        <img src={avatar} className="avatar-logo" alt="avatar" />
-        <button className="CambiarFoto">Cambiar Foto</button>
-        
-
-        <form onSubmit={handleSubmit}>
+    <div className="fondo-pantallaModificacion">
+      <div class="caja-centro">
+        <div className="top">
+          <img
+            src={LogoComunicarte}
+            className="Comunicarte-logo"
+            alt="LogoComunicarte"
+          />
+          <img 
+          src={avatar} 
+          className="Avatar-logo" 
+          alt="Avatar" 
+          />
+        </div>
+        <button className="botonCambiarFoto">Cambiar Foto</button>
+        <div className="InputsNombre">
           <div className="FirstName">
-            <form>
-                <input
-                  placeholder="Primer Nombre"
-                  type="text"
-                  value={Firstname}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              
-              {error && Firstname.length <= 0 ? (
-                <label className="nameLabel">Nombre no puede estar vacio</label>
-              ) : (
-                ""
-              )}
-            </form>
+            <input
+              placeholder="Primer Nombre"
+              type="text"
+              value={Firstname}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
 
-          <div className="MiddleName">
-            <form>
-              
-                <input
-                  placeholder="Segundo Nombre"
-                  type="text"
-                  value={Middlename}
-                  onChange={(e) => setMiddleName(e.target.value)}
-                />
-              
-            </form>
+          <div className="SecondName">
+            <input
+              placeholder="Segundo Nombre"
+              type="text"
+              value={Middlename}
+              onChange={(e) => setMiddleName(e.target.value)}
+            />
           </div>
+        </div>
 
-          <div className="FirstLastName">
-            <form>
-                <input
-                  placeholder="Primer Apellido"
-                  type="text"
-                  value={FirstLastname}
-                  onChange={(e) => setFirstLastName(e.target.value)}
-                />
-              
-              {error && FirstLastname.length <= 0 ? (
-                <label className="firstLastNameLabel">Apellido no puede estar vacio</label>
-              ) : (
-                ""
-              )}
-            </form>
-          </div>
-
-          <div className="SecondLastName">
-            <form>
-                <input
-                  placeholder="Segundo Apellido"
-                  type="text"
-                  value={SecondLastname}
-                  onChange={(e) => setSecondLastName(e.target.value)}
-                />
-              
-              {error && SecondLastname.length <= 0 ? (
-                <label className="secondLastNameLabel">Apellido no puede estar vacio</label>
-              ) : (
-                ""
-              )}
-            </form>
+        <div className="InputsApellido-Correo">
+          <div className="Apellidos">
+            <input
+              placeholder="Apellidos"
+              type="text"
+              value={Lastnames}
+              onChange={(e) => setLastNames(e.target.value)}
+            />
           </div>
 
           <div className="Correo">
-            <form>
-                <input
-                  placeholder="Correo Electronico"
-                  type="text"
-                  value={Correo}
-                  onChange={(e) => setCorreo(e.target.value)}
-                />
-              
-              {error && Correo.length <= 0 ? (
-                <label className="correoLabel">Correo no puede estar vacio</label>
-              ) : (
-                ""
-              )}
-            </form>
+            <input
+              placeholder="Correo"
+              type="text"
+              value={Correo}
+              onChange={(e) => setCorreo(e.target.value)}
+            />
           </div>
-
-          <div className="FechaNacimiento">
-            <form>
-                <input
-                  placeholder="Fecha de Nacimiento"
-                  type="text"
-                  value={FechaNacimiento}
-                  onChange={(e) => setFechaNacimiento(e.target.value)}
-                />
-              
-              {error && FechaNacimiento.length <= 0 ? (
-                <label className="fechaNacimientoLabel">Fecha Nacimiento no puede estar vacio</label>
-              ) : (
-                ""
-              )}
-            </form>
-          </div>
-          <button className="GuardarCambios">Guardar cambios</button>
-        </form>
-      </div>
-      {/*RECTANGULO INFERIOR ROSA*/}
-      <div className="RectanguloInferiorBlanco">
-        {/*MOVERNOS A VENTANA DE DATOS DEL PERFIL*/}
-        <Link to="/" className="MoverPerfil">
-          Datos de Perfil
-        </Link>
+        </div>
+        <div className="botonesModificacion">
+          <button className="botonModificacion">Volver</button>
+          <button className="botonModificacion">Guardar Cambios</button>
+        </div>
       </div>
     </div>
+    // <div className="App">
+    //   {/*RECTANGULO SUPERIOR ROSA*/}
+    //   <header className="RectanguloSuperiorRosa"></header>
+
+    //   {/*CUADRO CENTRADO DONDE VA TODA LA DATA*/}
+    //   <div class="CuadroCentrado">
+    //     <img src={avatar} className="avatar-logo" alt="avatar" />
+    //     <button className="CambiarFoto">Cambiar Foto</button>
+
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="FirstName">
+    //         <form>
+    //             <input
+    //               placeholder="Primer Nombre"
+    //               type="text"
+    //               value={Firstname}
+    //               onChange={(e) => setFirstName(e.target.value)}
+    //             />
+
+    //           {error && Firstname.length <= 0 ? (
+    //             <label className="nameLabel">Nombre no puede estar vacio</label>
+    //           ) : (
+    //             ""
+    //           )}
+    //         </form>
+    //       </div>
+
+    //       <div className="MiddleName">
+    //         <form>
+
+    //             <input
+    //               placeholder="Segundo Nombre"
+    //               type="text"
+    //               value={Middlename}
+    //               onChange={(e) => setMiddleName(e.target.value)}
+    //             />
+
+    //         </form>
+    //       </div>
+
+    //       <div className="FirstLastName">
+    //         <form>
+    //             <input
+    //               placeholder="Primer Apellido"
+    //               type="text"
+    //               value={FirstLastname}
+    //               onChange={(e) => setFirstLastName(e.target.value)}
+    //             />
+
+    //           {error && FirstLastname.length <= 0 ? (
+    //             <label className="firstLastNameLabel">Apellido no puede estar vacio</label>
+    //           ) : (
+    //             ""
+    //           )}
+    //         </form>
+    //       </div>
+
+    //       <div className="SecondLastName">
+    //         <form>
+    //             <input
+    //               placeholder="Segundo Apellido"
+    //               type="text"
+    //               value={SecondLastname}
+    //               onChange={(e) => setSecondLastName(e.target.value)}
+    //             />
+
+    //           {error && SecondLastname.length <= 0 ? (
+    //             <label className="secondLastNameLabel">Apellido no puede estar vacio</label>
+    //           ) : (
+    //             ""
+    //           )}
+    //         </form>
+    //       </div>
+
+    //       <div className="Correo">
+    //         <form>
+    //             <input
+    //               placeholder="Correo Electronico"
+    //               type="text"
+    //               value={Correo}
+    //               onChange={(e) => setCorreo(e.target.value)}
+    //             />
+
+    //           {error && Correo.length <= 0 ? (
+    //             <label className="correoLabel">Correo no puede estar vacio</label>
+    //           ) : (
+    //             ""
+    //           )}
+    //         </form>
+    //       </div>
+
+    //       <div className="FechaNacimiento">
+    //         <form>
+    //             <input
+    //               placeholder="Fecha de Nacimiento"
+    //               type="text"
+    //               value={FechaNacimiento}
+    //               onChange={(e) => setFechaNacimiento(e.target.value)}
+    //             />
+
+    //           {error && FechaNacimiento.length <= 0 ? (
+    //             <label className="fechaNacimientoLabel">Fecha Nacimiento no puede estar vacio</label>
+    //           ) : (
+    //             ""
+    //           )}
+    //         </form>
+    //       </div>
+    //       <button className="GuardarCambios">Guardar cambios</button>
+    //     </form>
+    //   </div>
+    //   {/*RECTANGULO INFERIOR ROSA*/}
+    //   <div className="RectanguloInferiorBlanco">
+    //     {/*MOVERNOS A VENTANA DE DATOS DEL PERFIL*/}
+    //     <Link to="/" className="MoverPerfil">
+    //       Datos de Perfil
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
--- src/Components/VentanaPerfil.js
+++ src/Components/VentanaPerfil.js
@@ -1,58 +1,129 @@
 import React from "react";
 import "./VentanaPerfil.css";
 import LogoComunicarte from "./LogoComunicarte.png";
+import avatar from "./avatar.png";
 import { FaBirthdayCake } from "react-icons/fa";
 import { FaAt } from "react-icons/fa";
 import { FaStackExchange } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export const VentanaPerfil = () => {
+  const navigate = useNavigate();
+  const { datosUsuario } = useContext(AuthContext);
+  const datosObtenidos = {
+    primer_nombre: "",
+    segundo_nombre: "",
+    apellidos: "",
+    correo_user: "",
+    fecha_de_nacimiento: "",
+    fecha_de_inicio: "",
+    puesto_empresa: "",
+  };
 
-  const [infoperfil, setInfoperfil] = useState([])
+  const [dataPerfilUsuario, setDataPerfilUsuario] = useState(datosObtenidos);
 
-  const cargarInfoUsuario = async() =>{
-    const response = await fetch('http://localhost:3000/')
-    const data = await response.json()
-    setInfoperfil(data)
-  }
-
+  const obtenerDatos = async () => {
+    const { correo } = datosUsuario;
+    console.log(correo);
+    const body = { correo: correo };
+    console.log(body);
+    try {
+      const respuesta = await fetch(
+        "https://comunicartewebapp-api.herokuapp.com/perfil_usuario",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      )
+        .then((respuesta) => respuesta.json())
+        .then((data) => {
+          setDataPerfilUsuario({
+            primer_nombre: data.primer_nombre,
+            segundo_nombre: data.segundo_nombre,
+            apellidos: data.apellidos,
+            correo_user: data.correo,
+            fecha_de_nacimiento: data.fecha_de_nacimiento,
+            fecha_de_inicio: data.fecha_de_inicio,
+            puesto_empresa: data.puesto_empresa,
+          });
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    cargarInfoUsuario()
-  }, [])
-
+    window.onbeforeunload = obtenerDatos();
+  }, []);
   return (
-    <div className="App">
-      <header className="ParteSuperiorRosa">
-        <img
-          src={LogoComunicarte}
-          className="Comunicarte-logo"
-          alt="LogoComunicarte"
-        />
-        <img src={LogoComunicarte} className="App-logo" alt="logo" />
-        <div className="name"></div>
-        <div className="puesto">Gerente</div>
-      </header>
+    <div className="fondo-pantalla">
+      <div class="caja-perfil">
+        <div className="top">
+          <img
+            src={LogoComunicarte}
+            className="Comunicarte-logo"
+            alt="LogoComunicarte"
+          />
 
-      <div className="ParteInferiorBlanca">
-        <p classname="fechanac">
-          <FaBirthdayCake />
-          Fecha de Nacimiento: 08/02/1993
-        </p>
-        <p classname="correo">
-          <FaAt />
-          pacoperez@comunicarte.hn
-        </p>
+          <img src={avatar} className="Avatar-logo" alt="Avatar" />
+          <h3>
+            <input
+              className="forms"
+              name="pnombre"
+              value={dataPerfilUsuario.primer_nombre}
+            />
+            <input
+              className="forms"
+              name="snombre"
+              value={dataPerfilUsuario.segundo_nombre}
+            />
+            <input
+              className="forms"
+              name="papellido"
+              value={dataPerfilUsuario.apellidos}
+            />
+          </h3>
+          <h3>
+            {" "}
+            <input
+              className="forms"
+              name="empresa"
+              value={dataPerfilUsuario.puesto_empresa}
+            />{" "}
+          </h3>
+        </div>
+
+        <div className="datosPerfil">
+          <p classname="fechanac">
+            <FaBirthdayCake />
+            <input
+              className="forms"
+              name="fechaNacimiento"
+              value={dataPerfilUsuario.fecha_de_nacimiento}
+            />
+          </p>
+
+          <p classname="correo">
+            <FaAt />
+            <input
+              className="forms"
+              name="correo"
+              value={dataPerfilUsuario.correo_user}
+            />
+          </p>
+        </div>
         <p classname="fechaini">
           <FaStackExchange />
-          Fecha de Inicio: 16/11/2022
+          <input
+            className="forms"
+            name="fechaIngreso"
+            value={dataPerfilUsuario.fecha_de_inicio}
+          />
         </p>
-      </div>
-      <div>
-        {/*MOVERNOS A VENTANA DE MODIFICACION DE DATOS*/}
-        <Link to="/Modificacion" className="MoverModificacion">
-          Modificar Datos
-        </Link>
+        <div className="botones">
+          <button className="boton">Volver</button>
+          <button className="boton">Editar Perfil</button>
+        </div>
       </div>
     </div>
   );
