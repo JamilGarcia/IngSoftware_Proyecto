4698ccfdf953c2dcebb1da62e11e7645f6109075
miguelblanco784 <miguelrobertoblanco@gmail.com>
2022-12-09 23:43:51 -0600

    Updated some formats

--- web-app/src/Componentes/Pantallas/VentanasGerente/FlujoEfectivo.js
+++ web-app/src/Componentes/Pantallas/VentanasGerente/FlujoEfectivo.js
@@ -1,10 +1,46 @@
 const FlujoEfectivo = () => {
-
   return (
     <>
-    <h1>Flujo Efectivo</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col"># De Referencia</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Concepto</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
     </>
-);
-}
+  );
+};
 
 export default FlujoEfectivo;
