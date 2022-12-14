731fad13dffe02fd32431d5e44c21ef970db0097
miguel <miguel@Miguels-MacBook-Air.local>
2022-12-14 00:50:46 -0600

    Inclusion de componente flujo de efectivo en gerente

--- web-app/src/Componentes/Pantallas/VentanasGerente/FlujoEfectivo.js
+++ web-app/src/Componentes/Pantallas/VentanasGerente/FlujoEfectivo.js
@@ -1,11 +1,36 @@
-
+import '../../../Hojas-de-estilo/FlujoEfectivo.css';
 import React from "react";
 const FlujoEfectivo = () => {
 
   return (
     <React.Fragment>
     <section>
-      <div>Flujo Efectivo</div>
+    <div className= "g_fe_fondo">
+        <div className="g_fe_caja">
+            <h2 className="h2">FLUJO EFECTIVO</h2>
+            <div className='divColumnasFe'>
+            <div>
+            <p className="tituloMOD">PRIMER NOMBRE</p>
+              <input type = "PrimerNombre" placeholder ="PrimerNombre" id="PrimerNombre" name="PrimerNombre" className="form-fe" />
+              <p className="tituloMOD">SEGUNDO NOMBRE</p>
+              <input type = "SegundoNombre" placeholder ="SegundoNombre" id="SegundoNombre" name="SegundoNombre" className="form-fe" />
+              <p className="tituloMOD">CORREO</p>
+              <input type = "Correo" placeholder ="Correo" id="Correo" name="Correo" className="form-fe" />
+            </div>
+            <div> 
+            <p className="tituloMOD">PRIMER NOMBRE</p>
+              <input type = "PrimerNombre" placeholder ="PrimerNombre" id="PrimerNombre" name="PrimerNombre" className="form-fe" />
+              <p className="tituloMOD">SEGUNDO NOMBRE</p>
+              <input type = "SegundoNombre" placeholder ="SegundoNombre" id="SegundoNombre" name="SegundoNombre" className="form-fe" />
+              <p className="tituloMOD">CORREO</p>
+              <input type = "Correo" placeholder ="Correo" id="Correo" name="Correo" className="form-fe" />
+            </div>
+            </div>
+            
+            
+            <button className='botonFlujoEfectivo'>Summit</button>
+        </div>
+    </div>
     </section>
   </React.Fragment>
 );
